ee33b395fe9092aa81beef70fa5a61d421ae7afc
ASDIO1 <diego.an.sanchez.a@gmail.com>
2021-03-11 22:38:20 -0400

    Headers listos para el readme

--- Mario Game/main.py
+++ Mario Game/main.py
@@ -1,1 +1,32 @@
 # Hello
+graph = {'A': ['B', 'C', 'E'],
+         'B': ['A', 'D', 'E'],
+         'C': ['A', 'F', 'G'],
+         'D': ['B'],
+         'E': ['A', 'B', 'D'],
+         'F': ['C'],
+         'G': ['C']}
+
+# JUST TESTING BFS
+
+
+def bfs(graph, initial):
+
+    visited = []
+
+    queue = [initial]
+
+    while queue:
+
+        node = queue.pop(0)
+        if node not in visited:
+
+            visited.append(node)
+            neighbours = graph[node]
+
+            for neighbour in neighbours:
+                queue.append(neighbour)
+    return visited
+
+
+print(bfs(graph, 'A'))
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # mario-map-diego-sanchez TEST
+
+## 1. Definicion del problem solver agent
+
+### 1.A Formulacion del objetivo
+
+### 1.B Formulacion del problema
+
+### 1.C Solucion del problema
+
+## 2. Algoritmo para la solucion
+
+### 2.A Justificacion de desicion
+
+## 3. Explicacion breve de la logica del Algoritmo
